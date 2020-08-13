8ade8760420dc6363bd64cef0374b46ab96b9f46
bangarangler <bangarangler@gmail.com>
2020-08-13 15:20:42 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+// Initializees your app with your bot token and signing secret
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log("⚡ Bolt app is running!");
+})();
